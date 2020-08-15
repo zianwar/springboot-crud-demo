22e46870b84ca866175b34e6179fdd4f311853cc
YEONCHEOL JANG <65603611+YeonCheolGit@users.noreply.github.com>
2020-08-15 18:56:27 +0900

    Update ProductServiceImpl.java
    commit

--- src/main/java/com/ensat/services/ProductServiceImpl.java
+++ src/main/java/com/ensat/services/ProductServiceImpl.java
@@ -24,8 +24,8 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public Product getProductById(Integer id) {
-        return productRepository.findOne(id);
+    public Optional<Product> getProductById(Integer id) {
+        return productRepository.findById(id);
     }
 
     @Override
@@ -35,7 +35,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public void deleteProduct(Integer id) {
-        productRepository.delete(id);
+        productRepository.deleteById(id);
     }
 
 }
